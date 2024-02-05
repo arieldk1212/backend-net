bc56f4fae2fa4d5f2094442eb08f703d0ad316fe
Ariel Kriheli <arieldk6@gmail.com>
2024-02-05 21:35:25 +0200

    added controllers

---
+++ api/Controllers/ExpenseController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using api.Data;
+
+namespace api.Controllers
+{
+    [Route("api/expense")]
+    [ApiController]
+    public class ExpenseController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ExpenseController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var expenses = _context.Expenses.ToList();
+
+            return Ok(expenses);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var expense = _context.Expenses.Find(id);
+            
+            if (expense == null) {
+                return NotFound();
+            }
+            return Ok(expense);
+        }
+    }
+}
